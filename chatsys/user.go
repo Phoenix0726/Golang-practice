882d8e5d75aa35bbd83e1e9655b341bd7ebd176b
long <caesar0726@126.com>
2024-05-13 19:23:49 +0800

    即时通讯系统-在线用户查询

--- chatsys/user.go
+++ chatsys/user.go
@@ -66,5 +66,20 @@ func (this *User) Offline() {
 
 // 处理消息
 func (this *User) DoMessage(msg string) {
-    this.server.BroadCast(this, msg)
+    if msg == "who" {
+        this.server.mapLock.Lock()
+        for _, user := range this.server.OnlineMap {
+            onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线"
+            this.SendMsg(onlineMsg)
+        }
+        this.server.mapLock.Unlock()
+    } else {
+        this.server.BroadCast(this, msg)
+    }
+}
+
+
+// 给当前 User 对应的客户端发送消息
+func (this *User) SendMsg(msg string) {
+    this.C <- msg
 }
